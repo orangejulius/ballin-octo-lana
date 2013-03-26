de6e535906c61afa6fb93fccd0aaa740dce04e50
Julian Simioni <julian.simioni@gmail.com>
2013-03-25 22:28:27 -0700

    Add new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BallinOctoLana::Application.config.secret_token = 'ef07408449a67a862ab907b64eadb6c10da7069db887be92c0397a80ec99dbc98a3823ebccda3bc2a4b9e4c0615f91ee5989dc354cf33aa9b9bb00e7d6139e79'
